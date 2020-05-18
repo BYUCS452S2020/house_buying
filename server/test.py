b0ab16dd2eef42f1d1a292a85b36d6d6f9d97c57
Thomas Cole <47041970+Prograndma@users.noreply.github.com>
2020-05-18 16:14:05 -0600

    Update test.py
    More testing.

--- server/test.py
+++ server/test.py
@@ -7,10 +7,29 @@ def main():
     if not connection.connect():
         connection.create_database()
 
-    connection.create_table("CREATE TABLE User (email VARCHAR(255), password VARCHAR(255))")
+    connection.create_table("CREATE TABLE User (email VARCHAR(255) NOT NULL,"
+                            " password VARCHAR(255) NOT NULL,"
+                            " PRIMARY KEY (email))")
     tables = connection.get_tables()
     print(tables)
-    
+    add_user = ("INSERT INTO User "
+                "(email, password) "
+                "VALUES (%(email)s, %(password)s)")
+
+    data_user = {
+        'email': 'email@g.com',
+        'password': '123abc',
+    }
+
+    connection.insert(add_user, data_user)
+
+    query = ("SELECT * FROM User "
+             "WHERE email=(%(email)s)")
+    query_data = {'email': 'email@g.com', }
+
+    results = connection.query(query, query_data)
+    print(results)
+
 
 if __name__ == "__main__":
     main()
