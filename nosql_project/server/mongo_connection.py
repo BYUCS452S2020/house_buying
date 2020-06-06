c1531c9231ada5feae1bf72d8a17a6a358a22624
mkbarnum <matthewkbarnum@gmail.com>
2020-06-06 16:30:16 -0500

    added working mongodb with listings and follow collection

--- nosql_project/server/mongo_connection.py
+++ nosql_project/server/mongo_connection.py
@@ -30,3 +30,51 @@ class MongoConnection:
             else:
                 print('successful login')
         return True
+
+    def addListing(self, email, address, price, num_bedrooms, num_bathrooms, home_type, image_url):
+        listings = self.db.listings
+
+        listing_document = {
+            "email": email,
+            "address": address,
+            "price": price,
+            "num_bedrooms": num_bedrooms,
+            "num_bathrooms": num_bathrooms,
+            "home_type": home_type,
+            "image_url": image_url
+        }
+        listings.insert_one(listing_document)
+
+        return 'Saved Successfully'
+    
+    def addFollower(self, email, follow_email):
+        follow = self.db.follow
+
+        follow_document = {
+            "email": email,
+            "follow_email": follow_email
+        }
+        follow.insert_one(follow_document)
+
+        return True
+
+
+    def checkFollowingEmail(self, follow_email):
+        user = self.db.user
+        user_document = user.find_one({"email": follow_email})
+        if user_document is None:
+            return False
+        else:
+            return True
+
+
+    def getFollowers(self, email):
+        follow = self.db.follow
+
+        return follow.find({"email": email})
+
+
+    def getListings(self, email):
+        listings = self.db.listings
+
+        return listings.find({"email": email})
--- nosql_project/server/server.py
+++ nosql_project/server/server.py
@@ -37,12 +37,77 @@ def login():
         return jsonify({"error": "Invalid login credentials"}), 403
 
 
+@app.route("/listing", methods=['POST'])
+def listing():
+    data = request.get_json()
+    if data and "email" in data:
+        email = data["email"]
+    if data and "address" in data:
+        address = data["address"]
+    if data and "price" in data:
+        price = data["price"]
+    if data and "num_bedrooms" in data:
+        num_bedrooms = data["num_bedrooms"]
+    if data and "num_bathrooms" in data:
+        num_bathrooms = data["num_bathrooms"]
+    if data and "home_type" in data:
+        home_type = data["home_type"]
+    if data and "image_url" in data:
+        image_url = data["image_url"]
+
+    message = database.addListing(email, address, price, num_bedrooms, num_bathrooms, home_type, image_url)
+
+    if message == 'Saved Successfully':
+        return jsonify({"message": message}), 200
+    else:
+        return jsonify({"message": "Unable to save listing"}), 404
+
+
+@app.route("/toFollow", methods=['POST'])
+def to_follow():
+    data = request.get_json()
+    user = ''
+    person_followed = ''
+    if data and "user" in data:
+        user = data["user"]
+    if data and "person_followed" in data:
+        person_followed = data["person_followed"]
+    if user == person_followed:
+        return jsonify({"message": f"You can't follow yourself."})
+
+    results = database.checkFollowingEmail(person_followed)
+    if results == False:
+        return jsonify({"message": f"{person_followed} is not an email in the database."})
+
+    if not database.addFollower(user, person_followed):
+        return jsonify({"message": "Unable to complete request"})
+
+    return jsonify({"message": "You now follow " + person_followed})
+
+
+@app.route("/following/<email>", methods=['GET'])
+def following(email):
+    results = database.getFollowers(email)
+    list_following = []
+
+    for obj in results:
+        list_following.append(obj['follow_email'])
+        
+    return jsonify({"following": list_following}), 200
+
+
+@app.route("/getListings/<email>", methods=['GET'])
+def get_listings(email):
+    results = database.getListings(email)
+    result_json = []
+    for obj in results:
+        temp_listing = Listing(obj['address'],obj['email'], obj['price'],obj['num_bedrooms'],obj['num_bathrooms'],obj['home_type'],obj['image_url'])
+        result_json.append(json.dumps(temp_listing.__dict__))
+
+    listings = result_json
+    # listings_json = json.dumps(listings)
+    return jsonify({"listings": [listings]}), 200
+
 if __name__ == '__main__':
-    # global connection
-    # connection = SQLConnection(host="localhost", user="root", passwd="password", port=3306, db='testdb')
-    # if not connection.start_up():
-    #     print(f"Couldn't connect to SQL data base")
-    #     exit(-1)
-    # put_dummy_data()
 
     app.run(debug=True)
